0ae5e0556a195de17a59977dff57b6aa1ec399e1
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-01 19:10:37 -0500

    start Program logic

---
+++ RestaurantReview/UI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMenu restMenu = new MainMenu();
+            bool repeat = true;
+            string currentMenu = "MainMenu";
+
+
+
+            while (repeat)
+            {
+
+
+                Console.Clear();
+                restMenu.Menu();
+                currentMenu = restMenu.YourChoice();
+            }
+        }
+    }
+}
