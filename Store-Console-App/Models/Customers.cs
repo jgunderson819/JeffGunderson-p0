0b9dd39808d6a79657ca8daab6e202b02f7f7599
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-03 12:54:18 -0500

    Added Customers Model

---
+++ Store-Console-App/Models/Customers.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Models
+{
+    public class Customers
+    {
+        private string _city;
+        public Customers()
+        {
+            Name = "Default";
+        }
+
+        public Customers(string p_name)
+        {
+            Name = p_name;
+        }
+        public string Name { get; set; }
+        public string City
+        {
+
+            get
+            {
+                return _city;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    //This will throw an exception whenever you try to store the city property with numbers
+                    throw new Exception("City can only hold letters!");
+                }
+
+                _city = value;
+            }
+
+        }
+        public string State { get; set; }
+
+        public List<Customers> Reviews { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, City: {City}, State: {State}, Email: {Email}. Orders: {OrdersList}";
+        }
+    }
+}
