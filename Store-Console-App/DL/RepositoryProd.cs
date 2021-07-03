ec69425dada013c58597dcec517c46c196dcbd1d
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-03 15:15:29 -0500

    Added RepositoryProd

---
+++ Store-Console-App/DL/RepositoryProd.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Modelc;
+
+namespace DL
+{
+    public class RepositoryProd : IRepositoryProd
+    {
+        private const string _filePath = "./../RRDL/Database/Products.json";
+        private string _jsonString;
+        public Products AddProducts(Products p_prod)
+        {
+            List<Products> listOfProducts = this.GetAllProducts();
+            listOfProducts.Add(p_rest);
+
+            _jsonString = JsonSerializer.Serialize(listOfProducts, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, _jsonString);
+            return p_prod;
+        }
+
+        public List<Products> GetAllProducts()
+        {
+            try
+            {
+                _jsonString = File.ReadAllText(_filePath);
+            }
+            catch (System.Exception)
+            {
+                throw new Exception("File path is invalid");
+            }
+
+            //This will return a list of restaurant from the jsonString that came from 
+            return JsonSerializer.Deserialize<List<Products>>(_jsonString);
+        }
+
+        public Products GetProducts(Products p_prod)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
