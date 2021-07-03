d30a7540d2b794efd824703c4e12c26d23217d54
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-03 09:31:32 -0500

    Added to program logic

--- Store-Console-App/UI/Program.cs
+++ Store-Console-App/UI/Program.cs
@@ -6,7 +6,32 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            restMenu.Menu();
+            currentMenu = restMenu.YourChoice();
+
+            switch (currentMenu)
+            {
+                case MenuType.MainMenu:
+                    restMenu = menuFactory.GetMenu(MenuType.MainMenu);
+                    break;
+                case MenuType.StoreMenu:
+                    restMenu = menuFactory.GetMenu(MenuType.RestaurantMenu);
+                    break;
+                case MenuType.ShowStoreMenu:
+                    restMenu = menuFactory.GetMenu(MenuType.ShowRestaurantMenu);
+                    break;
+                case MenuType.AddStoreMenu:
+                    restMenu = menuFactory.GetMenu(MenuType.AddRestaurantMenu);
+                    break;
+                case MenuType.Exit:
+                    repeat = false;
+                    break;
+                default:
+                    Console.WriteLine("Cannot process what you want please try again");
+                    break;
+            }
         }
     }
+}
 }
